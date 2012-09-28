efd57572c0ddb93456c0dd71f3cea0d73f4d8e6d
Aaron Smith <gngrwzrd@gmail.com>
2012-09-28 00:28:20 -0700

    Took out some older coverage / unit testing vars internally.

--- src/gwrl/event_private.h
+++ src/gwrl/event_private.h
@@ -3,13 +3,8 @@
 extern int asserts_var1;
 extern bool asserts_var2;
 typedef enum assert_codes {
-	gwrl_create_fail = 10,
-	gwrl_create_gatherfncs_fail = 11,
-	gwrl_evt_create_fail = 12,
-	gwrl_src_time_create_fail = 13,
-	gwrl_src_file_create_fail = 14,
-	gwrlbkd_init_fail = 20,
-	gwrlbkd_no_sleep_assert_true = 21,
+	gwrl_assert_wake_reinit = 10,
+	gwrlbkd_no_sleep_assert_true = 20
 } assert_codes;
 #endif
 
