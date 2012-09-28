60bf0790537abac10dbe7ff29d0cd4a2961741e8
Aaron Smith <gngrwzrd@gmail.com>
2012-09-28 00:34:19 -0700

    more assertions for reactor options

--- test/reactor_options.c
+++ test/reactor_options.c
@@ -2,26 +2,24 @@
 #include "gwrl/event.h"
 
 int main(int argc, char ** argv) {
-
 	gwrl * rl = gwrl_create();
-	//assert(rl->gatherfncs == NULL);
 	
 	gwrl_options opts = GWRL_DEFAULT_OPTIONS;
 	opts.gwrl_gather_funcs_max = 10;
-	
 	gwrl_set_options(rl,&opts);
 	assert(rl->gatherfncs != NULL);
 	
-	//assert(test_var1 == 1);
-	//assert(test_var2 == 1);
-
 	opts.gwrl_gather_funcs_max = 20;
 	gwrl_set_options(rl,&opts);
+	assert(rl->gatherfncs != NULL);
 
-	//assert(test_var3 == 1);
-	//assert(test_var1 == 2);
-	
-	assert(rl->gatherfncs != NULL);	
+	gwrl_options opts2 = GWRL_DEFAULT_OPTIONS;
+	free(rl->gatherfncs);
+	rl->gatherfncs = NULL;
+	rl->options.gwrl_gather_funcs_max = 0;
+	opts2.gwrl_gather_funcs_max = 10;
+	gwrl_set_options(rl,&opts2);
+	assert(rl->gatherfncs != NULL);
 
 	return 0;
 }
