5ad3dcf28697104488f17af5699ff0a94c9ea4e4
Aaron Smith <gngrwzrd@gmail.com>
2012-10-07 15:45:00 -0700

    Added subdirectory option for coverage script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-../../scripts/runtests.py -s ./ -g ../gcov_files/ -t ./test/ -x ../../scripts/
+../../scripts/runtests.py -s ./ -g ../gcov_files/$1/ -t ./test/ -x ../../scripts/
 ../../scripts/lcov.py -g ../gcov_files/ -o ../../docs/lcov -r _OSByteOrder.h -r _string.h
